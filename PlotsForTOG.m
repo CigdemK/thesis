cf03248560c57f240eeebab91a65aa98739b081d
CigdemK <cigdem.kocberber@gmail.com>
2014-11-30 11:45:05 +0200

    PlotsForTOG: Comparison of Retarget Methods plot added.

--- PlotsForTOG.m
+++ PlotsForTOG.m
@@ -1,12 +1,33 @@
+%% Init
 clc,clear
-
-% Plot dispersion curve
 moviePath = 'F:\Thesis\Hollywood2-actions\Hollywood2\AVIClips\actioncliptest00001.avi';
 shotStart = 48;
 shotEnd = 95;
 
 video = VideoReader( moviePath );
 frames = read(video);
+
+%% Plot comparison line graph
+RetargetingMethods = {'Rubinstein2008','Wang2011','Yan2013','Nei2013','Linear','Cigdem'};
+ComparisonMethods = {'Compression Ratio','Waving','Blur','Sharpness','Jitter'};
+
+nrRetMethods = size(RetargetingMethods,2);
+nrCompMethods = size(ComparisonMethods,2);
+
+Scores = rand([nrRetMethods nrCompMethods]).*100; % #Retmethods x #Compmethods
+
+colorArr = {'b','g','k','r','m','c','y'}; 
+figure;
+for i = 1:nrRetMethods
+    plot(Scores(i,:), 1:nrCompMethods,'Color',colorArr{mod(i,nrRetMethods)+1},'LineWidth',3);
+    hold on;
+end
+set(gca, 'XTick', 0:20:100 , 'YTickLabel', ComparisonMethods);
+title(['Comparison of Retaregeting Methods'],'FontWeight','Bold');
+xlabel('Scores','FontWeight','Bold');
+ylabel('Comparison Metrics','FontWeight','Bold');
+legend(RetargetingMethods,'location','eastoutside');
+%% Plot dispersion curve
 
 dispersion = zeros(shotEnd-shotStart+1,1);
 load('videoSaliency.mat');
@@ -30,5 +51,13 @@ for i = shotStart:shotEnd %1:video.NumberOfFrames
 end
     
 figure;
-%     plot(1:video.NumberOfFrames,dispersion);
-plot(shotStart:shotEnd,dispersion,'LineWidth',1);
+plot(shotStart:shotEnd-1,dispersion(1:end-1),'LineWidth',2);
+ylim([1 max(dispersion)]);
+xlabel('Frames','FontWeight','Bold');
+ylabel('Dispersion','FontWeight','Bold');
+hold on;
+p = polyfit(1:(shotEnd-shotStart),dispersion(1:end-1)',3);
+y = polyval(p,1:shotEnd-shotStart);
+plot(shotStart:shotEnd-1,y,'LineWidth',2,'Color','r');
+
+
